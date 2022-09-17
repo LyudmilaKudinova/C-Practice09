8269993130b4372f6eb472c37907cfc325bcd34f
lyudmila <kudinova@alio-soft.ru>
2022-09-17 21:38:59 +0400

    Сумма натуральных элементов в промежутке от M до N

---
+++ C#Pr09Task002/Program.cs
@@ -0,0 +1,8 @@
+﻿//Задача 66: Задайте значения M и N. 
+//Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+int SumRec(int a, int b)
+{
+    if (a <= b) return SumRec(a, b - 1) + b;
+    else return 0;
+}
+Console.WriteLine(SumRec(1, 12));
