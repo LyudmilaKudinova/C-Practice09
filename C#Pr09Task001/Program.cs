0db9c6584b401974a8367c6cf82fccc5dec16a37
lyudmila <kudinova@alio-soft.ru>
2022-09-17 21:36:46 +0400

    Натуральные числа в промежутке от M до N

---
+++ C#Pr09Task001/Program.cs
@@ -0,0 +1,7 @@
+﻿//Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+string NumbersRec(int a, int b)
+{
+    if (a <= b) return NumbersRec(a, b - 1) + $"{b} ";
+    else return String.Empty;
+}
+Console.WriteLine(NumbersRec(1, 12));
